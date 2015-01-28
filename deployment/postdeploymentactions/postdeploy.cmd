4c9f9016235d477a0f90f5d07568f5f096791549
Daniel Larsen <daniel@larsen.net.nz>
2015-01-28 22:10:56 +1300

    debugging postdeploy.cmd

--- deployment/postdeploymentactions/postdeploy.cmd
+++ deployment/postdeploymentactions/postdeploy.cmd
@@ -1,13 +1,15 @@
+@echo on
+
 IF EXIST "%DEPLOYMENT_TARGET%\Gruntfile.js" (
   pushd "%DEPLOYMENT_TARGET%"
-  @echo on
+  
   npm install -g grunt-cli
   IF !ERRORLEVEL! NEQ 0 goto error
   npm install
   IF !ERRORLEVEL! NEQ 0 goto error
   grunt --no-color
   IF !ERRORLEVEL! NEQ 0 goto error
-  @echo off
+  
   popd
 )
 
@@ -15,6 +17,8 @@ goto end
 
 :error
 echo An error occured in the Post Deployment script
+@echo off
 exit /b 1
 
 :end
+@echo off
